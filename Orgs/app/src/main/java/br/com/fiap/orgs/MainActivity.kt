f921c2bffeb74c949d928e9e5b65c697d8a91d3a
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-14 22:20:55 -0300

    Aula 14 de março

--- Orgs/app/src/main/java/br/com/fiap/orgs/MainActivity.kt
+++ Orgs/app/src/main/java/br/com/fiap/orgs/MainActivity.kt
@@ -1,6 +1,7 @@
 package br.com.fiap.orgs
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,5 +16,15 @@ import br.com.fiap.orgs.ui.theme.OrgsTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        /* Binding: processo de cinculacao do codigo fonte
+        com o arquivo de layout
+        * */
+
+        val nome = findViewById<TextView>(R.id.nome)
+        nome.setText("Cesta de Frutas")
+        val descricao = findViewById<TextView>(R.id.descricao)
+        descricao.setText("Laranja, uva e maca")
+        val valor = findViewById<TextView>(R.id.valor)
+        valor.setText("Laranja, uva e maca")
     }
 }
